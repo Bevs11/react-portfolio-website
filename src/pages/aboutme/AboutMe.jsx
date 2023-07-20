df3cdc0633d88eafae01cf04bf6cbd485ede6f7f
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2023-07-20 09:09:49 +0800

    fix: changed google drive link to resume

--- src/pages/aboutme/AboutMe.jsx
+++ src/pages/aboutme/AboutMe.jsx
@@ -35,7 +35,7 @@ const AboutMe = () => {
             </button>
           </a>
           <a
-            href="https://drive.google.com/file/d/11ACHpopAnqNDaTzZUbVjVpVz50a_LgB-/view?usp=sharing"
+            href="https://drive.google.com/file/d/1Yov41EAb3ubDuIWINTy2wnPCCiyLiiLW/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
           >
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
             </button>
           </a>
           <a
-            href="https://drive.google.com/file/d/11ACHpopAnqNDaTzZUbVjVpVz50a_LgB-/view?usp=sharing"
+            href="https://drive.google.com/file/d/1Yov41EAb3ubDuIWINTy2wnPCCiyLiiLW/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
           >
